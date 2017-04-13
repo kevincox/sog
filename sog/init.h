a634f8a653ac9a1d6647077e398062966acd136c
Kevin Cox <kevincox@kevincox.ca>
2017-04-13 23:15:32 +0100

    Add docs for sog::init().

--- sog/init.h
+++ sog/init.h
@@ -4,6 +4,15 @@
 #include "sink.h"
 
 namespace sog {
+	/** Initalize sog.
+	 * 
+	 * It is an error to log a message before init is called.
+	 * 
+	 * It is an error to call this method twice.
+	 * 
+	 * @param The sink to send logs to. If null a sog::PrettySink to stderr
+	 *	will be used.
+	 */
 	void init(Sink *sink=nullptr);
 }
 
